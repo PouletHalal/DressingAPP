0534d089be320719c161898a5465dc540c6a9333
PouletHalal <sami12022005@yahoo.fr>
2024-10-04 17:49:04 +0200

    feat: add basic pages and scroll between pages

---
+++ lib/pages/clothepage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Clothepage extends StatefulWidget {
+  const Clothepage({super.key});
+
+  @override
+  State<Clothepage> createState() => _ClothepageState();
+}
+
+class _ClothepageState extends State<Clothepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.purple,
+    );
+  }
+}
